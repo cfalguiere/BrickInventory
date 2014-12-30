5c9b8bbcffcbcf06dda631e6ca8ffb86a524e2c9
Claude Falguiere <cfalguiere@gmail.com>
2014-12-30 22:03:11 +0100

    vagrant file now installs the packages

--- install-env.bash
+++ install-env.bash
@@ -1,16 +1,51 @@
-#!/bin/bash
+#!/bin/bash -e
+
+cd /vagrant
+
+echo "Starting at $( date )"
+
+echo '######################'
+echo 'Updating apt'
+echo '######################'
+
 sudo apt-get update
+
+echo '######################'
+echo 'Installing node packages'
+echo '######################'
+
 sudo apt-get -y install git
 sudo apt-get -y install nodejs
-#sudo apt-get install nodejs-legacy
+sudo apt-get install nodejs-legacy
 sudo apt-get -y install npm
-sudo npm install -g grunt-cli
-sudo npm install -g bower
+
+echo '######################'
+echo 'Installing yeoman'
+echo '######################'
+
+npm install -g yo bower grunt-cli
+npm install -g generator-angular
+
+echo '######################'
+echo 'Installing http-server'
+echo '######################'
 
 # to be able to run npm start and bind eth0 instead of localhost
 sudo npm install -g http-server
 
+echo '######################'
+echo 'Installing firefox'
+echo '######################'
+
 # used by karma tests
 sudo apt-get -y install firefox
 
-#sudo npm install karma-phantomjs-launcher --save-dev
+echo '######################'
+
+echo "Ending at $( date )"
+
+exit
+
+
+
+
