454c56024787a3982d6c3cfd061dab4d36358728
Claude Falguiere <cfalguiere@gmail.com>
2015-01-02 12:35:32 +0100

    added a test on shapeFilter when unselected

--- brickInventory/test/unit/bricks.controllers.js
+++ brickInventory/test/unit/bricks.controllers.js
@@ -3,16 +3,28 @@ describe('bricksController test', function(){
     beforeEach(module('BrickInventoryApp'));
 
 
-    it('should set shapeFilter to true for each Brick and false for other shapes', inject(function($controller) {
-      var scope = {}
-      var ctrl = $controller('bricksController', {$scope:scope});
-      var bricksList = scope.bricksList;
-      scope.filterByShape("Brick");
+    describe('shapeFilter is used ', function(){
 
-      expect(bricksList[0].shapeFilter).toBe( false );
-      expect(bricksList[4].shapeFilter).toBe( true );
-    }))
+        it('should set all shapeFilters to true if unselected', inject(function($controller) {
+          var scope = {}
+          var ctrl = $controller('bricksController', {$scope:scope});
+          var bricksList = scope.bricksList;
+          scope.filterByShape(null);
 
+          expect(bricksList[0].shapeFilter).toBe( true );
+          expect(bricksList[4].shapeFilter).toBe( true );
+        }))
+
+        it('should set shapeFilter to true for Brick and false for other shapes', inject(function($controller) {
+          var scope = {}
+          var ctrl = $controller('bricksController', {$scope:scope});
+          var bricksList = scope.bricksList;
+          scope.filterByShape("Brick");
+
+          expect(bricksList[0].shapeFilter).toBe( false );
+          expect(bricksList[4].shapeFilter).toBe( true );
+        }))
+    })
 
     it('should set colorFilter to true for each Red brick and false for other colors', inject(function($controller) {
       var scope = {}
