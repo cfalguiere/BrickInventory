58608754ef7094086519b28081ad25ab124d2d0a
Claude Falguiere <cfalguiere@gmail.com>
2015-01-03 17:33:32 +0100

    improved main view test

--- brickInventory/test/e2e/mainView.e2e-test.js
+++ brickInventory/test/e2e/mainView.e2e-test.js
@@ -1,23 +1,94 @@
 'use strict';
-describe('BrickInventory End-2-End App', function() {
-
-  describe('Main view', function() {
+describe('Main view End-2-End', function() {
+    var brickTable
 
     beforeEach(function() {
-      browser.get('index.html');
-      //browser.get('app/index.html');
-    });
+      browser.get('index.html')
+
+      brickTable = element.all(by.repeater('brick in bricksList'))
+    })
 
 
-    it('should show all the bricks', function() {
+    describe('When Main view appears', function() {
 
-      var brickList = element.all(by.repeater('brick in bricksList'));
+      it('should show all the bricks', function() {
 
-      expect(brickList.count()).toBe(16);
-   });
+        expect(brickTable.count()).toBe(5)
 
-    /*
+      })
 
+      it('should have a colorSelecter and colors', function() {
+
+        var colorSelecter = element(by.model('selectedColor'))
+        expect(colorSelecter).toBeDefined()
+        var colorSelecterItem = element(by.cssContainingText('option', 'Red'))
+        expect( colorSelecterItem.getText() ).toBe( "Red" )
+
+      })
+
+
+      it('should have a shapeSelecter and shapes', function() {
+
+        var shapeSelecter = element(by.model('selectedShape'))
+        expect(shapeSelecter).toBeDefined()
+        var shapeSelecterItem = element(by.cssContainingText('option', 'Robotics'))
+        expect( shapeSelecterItem.getText() ).toBe( "Robotics" )
+
+      })
+
+
+    })
+
+    describe('When user clicks Red', function() {
+
+      it('should narrow the list of brick to Red bricks', function() {
+
+         var total = brickTable.count()
+
+         var redColor = element(by.cssContainingText('option', 'Red'))
+         redColor.click()
+
+         //expect( brickList.count() ).toBeLessThan( total )
+         // does not work has items are still in the table, they are just hidden
+
+         var displayedElements = brickTable.filter(function(row) {
+             return row.isDisplayed()
+         })
+
+         expect( displayedElements.count() ).toBeLessThan( total )
+
+         displayedElements.map( function(element) {
+           expect(element.getText()).toContain('Red');
+         })
+
+      })
+
+   })
+
+   describe('When user clicks Robotics', function() {
+
+      it('should narrow the list of brick to Robotics', function() {
+
+         var total = brickTable.count()
+
+         var roboticsShape = element(by.cssContainingText('option', 'Robotics'))
+         roboticsShape.click()
+
+         var displayedElements = brickTable.filter(function(row) {
+             return row.isDisplayed()
+         })
+
+         expect( displayedElements.count() ).toBeLessThan( total )
+
+         displayedElements.map( function(element) {
+           expect(element.getText()).toContain('Robotics');
+         })
+
+      })
+
+   })
+
+   /*
     it('should filter the phone list as a user types into the search box', function() {
 
       var phoneList = element.all(by.repeater('phone in phones'));
@@ -33,5 +104,4 @@ describe('BrickInventory End-2-End App', function() {
       expect(phoneList.count()).toBe(2);
     });
     */
-  });
 });
